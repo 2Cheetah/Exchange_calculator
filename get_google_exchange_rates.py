381efb30e93bccd1ab011ebcf1f9896b8bb4a1f6
2Cheetah <fckninja@gmail.com>
2022-11-30 14:56:11 +0800

    Code clean-up

--- get_google_exchange_rates.py
+++ get_google_exchange_rates.py
@@ -5,18 +5,21 @@ import sys
 
 
 def get_exchange_rate_from_google_finance(cur1="usd", cur2="myr"):
+    """Parser of google finance site to extract exchange rate of provided currency pair"""
     currency1 = cur1.upper()
     currency2 = cur2.upper()
     url = f'https://www.google.com/finance/quote/{currency1}-{currency2}'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
     exchange_rate = soup.find(attrs={"data-source": f"{currency1}", "data-target": f"{currency2}"}).span.string.strip()
+    # Get rid of thousands delimiter, e.g. 16,400.50 --> 16400.50
     exchange_rate = exchange_rate.replace(",","")
     return float(exchange_rate)
 
 
 if __name__ == '__main__':
     try:
+        # The script can be called with two arguments to get exchange rate. For instance, "./get_google_exchange_rates.py usd myr"
         cur1 = sys.argv[1]
         cur2 = sys.argv[2]
         exchange_rate = get_exchange_rate_from_google_finance(cur1, cur2)
--- main.py
+++ main.py
@@ -17,18 +17,22 @@ Another option is to use the bot as an exchange calculator. Try typing '105.33 u
 
 @dp.message_handler()
 async def exchange_rate(message: types.Message):
+   # Check if any amount for conversion is provided
    exchange_amount = re.findall(r"\d+\.?,?\d*", message.text)
+   # Try to get a currency/cryptocurrency pair
    currency_list = re.findall(r"[a-zA-Z]{4}|[a-zA-Z]{3}", message.text)
    try:
       cur1 = currency_list[0].upper()
       cur2 = currency_list[1].upper()
-      await message.answer(f"Trying to fetch exchange rate for a pair {cur1}-{cur2} from Google Finance")
+      await message.answer(f"Fetching exchange rate for a pair {cur1}-{cur2} from Google Finance")
       try:
          exchange_rate = get_exchange_rate_from_google_finance(cur1, cur2)
          reply = f"Exchange rate for {cur1}-{cur2} is {exchange_rate}"
          await message.answer(reply)
+         # Convert provided amount from currency1 to currency2. In case of a float with ',' delimiter, it is replaced with '.'
          if exchange_amount:
-            await message.answer(f"{exchange_amount[0].replace(',','.')} of {cur1} = {float(exchange_amount[0].replace(',','.'))*exchange_rate} of {cur2}")
+            exchange_amount = float(exchange_amount[0].replace(',','.'))
+            await message.answer(f"{exchange_amount} {cur1} = {exchange_amount*exchange_rate} {cur2}")
       except:
          await message.answer(f"Can't process provided data \"{message.text}\"")
    except IndexError:
